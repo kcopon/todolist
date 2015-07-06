f7c8f05c5399a74c9606bda2b3a9bcb3396c1145
Kristoffer <justicecopon@gmail.com>
2015-07-05 18:56:49 -0700

    third commit 7/5/15

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "application#hello"
   
   devise_for :users
+    resources :users, only: [:update]
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
