9e1ee2c0a4e602f2003a15e40252ddde99181c20
Kristoffer <justicecopon@gmail.com>
2015-07-05 18:42:51 -0700

    Configured app for Production emails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
  
-  protected
+  rescue_from Pundit::NotAuthorizedError do |exception|
+    redirect_to root_url, alert: exception.message
+  end
 
   def hello
     render text: "<h1>Hello</h1><p>Welcome home</p>"
   end
+
+  protected
 
   def after_sign_in_path_for(resource)
     welcome_index_path
